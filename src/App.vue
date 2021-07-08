ae713fe5ceabc6aa07da77f915abfde5d0edddbd
WEBAN-HUB <zkzk1231@gmail.com>
2021-07-09 03:20:21 +0900

    components props

--- src/App.vue
+++ src/App.vue
@@ -5,41 +5,48 @@
       color="primary"
       dark
     >
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-toolbar-title>Page Title</v-toolbar-title>
-    <v-spacer/>
-    <v-btn icon to="/about">
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
-
-    <v-btn icon to="/">
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <site-title :title="title"></site-title>
+      <v-spacer/>
     </v-app-bar>
-
-    <v-main>
-      <router-view></router-view>
-    </v-main>
-
-    <v-footer
-      app
-      color="primary"
-      dark
-      absolute
-    >
-    <v-spacer></v-spacer>
-    <div>&copy; {{new Date().getFullYear()}}</div>
-    </v-footer>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <site-menu></site-menu>
+    </v-navigation-drawer>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <site-footer :footer="footer"></site-footer>
   </v-app>
 </template>
 
 <script>
+import SiteTitle from '@/views/site/title'
+import SiteFooter from '@/views/site/footer'
+import SiteMenu from '@/views/site/menu'
 
 export default {
+  components: { SiteTitle, SiteFooter, SiteMenu },
   name: 'App',
-
-  data: () => ({
-    //
-  })
+  data () {
+    return {
+      drawer: false,
+      items: [],
+      title: '나의 타이틀입니다',
+      footer: '푸터입니다'
+    }
+  },
+  mounted () {
+  }
 }
 </script>
---
+++ src/views/site/footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-footer
+      app
+      color="primary"
+      dark
+      absolute
+      footer="footer"
+    >
+    <v-spacer></v-spacer>
+    <div>&copy; {{new Date().getFullYear() + ' ' + footer}}</div>
+    </v-footer>
+</template>
+
+<script>
+
+export default {
+  props: ['footer']
+}
+
+</script>
