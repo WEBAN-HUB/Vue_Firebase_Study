378e18dd748afecdaf618ca16447f2b6a36fe22d
WEBAN-HUB <zkzk1231@gmail.com>
2021-07-09 17:24:28 +0900

    Firebase 연동 및 에러 처리(vuetify-toast-snackbar)

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <v-app-bar-nav-icon @click="drawer = !drawer" />
-      <site-title :title="title"></site-title>
+      <site-title :title="site.title"></site-title>
       <v-spacer/>
     </v-app-bar>
     <v-navigation-drawer app v-model="drawer">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider></v-divider>
-      <site-menu></site-menu>
+      <site-menu :items=site.menu></site-menu>
     </v-navigation-drawer>
     <v-content>
       <router-view/>
     </v-content>
-    <site-footer :footer="footer"></site-footer>
+    <site-footer :footer="site.footer"></site-footer>
   </v-app>
 </template>
 
@@ -41,12 +26,54 @@ export default {
   data () {
     return {
       drawer: false,
-      items: [],
-      title: '나의 타이틀입니다',
-      footer: '푸터입니다'
+      site: {
+        menu: [
+          {
+            title: 'home',
+            icon: 'mdi-home',
+            subItems: [
+              {
+                title: 'Dashboard',
+                to: '/'
+              },
+              {
+                title: 'About',
+                to: '/about'
+              }
+            ]
+          },
+          {
+            title: 'about',
+            active: true,
+            icon: 'mdi-account',
+            subItems: [
+              {
+                title: 'xxx',
+                to: '/xxx'
+              }
+            ]
+          }],
+        title: '나의 타이틀입니다',
+        footer: '푸터입니다'
+      }
     }
   },
-  mounted () {
+  created () {
+    this.subscribe()
+  },
+  methods: {
+    subscribe () {
+      this.$firebase.database().ref().child('site').on('value', (sn) => {
+        const v = sn.val()
+        if (!v) {
+          this.$firebase.database().ref().child('site').set(this.site)
+        }
+        this.site = v
+      }, (e) => {
+        console.log(e.message)
+      })
+    }
+
   }
 }
 </script>
